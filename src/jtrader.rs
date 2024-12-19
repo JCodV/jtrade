856d9af8550bdc02fc356398a2efd7a24db6ebd3
JCodV <154752793+JCodV@users.noreply.github.com>
2024-12-19 18:32:47 -0500

    added structs to store the increase/decrease of a stock within a given period

--- src/jtrader.rs
+++ src/jtrader.rs
@@ -11,10 +11,36 @@ pub enum Ticker {
     TSLA,
 }
 
+pub enum IncreaseOrDecrease {
+    UNCHANGED,
+    INCREASE,
+    DECREASE,
+}
+
+pub struct StockStats {
+    inc_or_dec: IncreaseOrDecrease,
+    percent_change: f32,
+}
+
+impl Default for StockStats {
+    fn default() -> Self {
+        StockStats {
+            inc_or_dec: IncreaseOrDecrease::UNCHANGED,
+            percent_change: 0.0,
+        }
+    }
+}
+
 pub struct JTrader {
     api_key: String,
     selected_stock: Ticker,
     doc_stock_data: Vec<DailyOpenClose>,
+
+    // stats
+    stats_yesterday: StockStats,
+    stats_week: StockStats,
+    stats_month: StockStats,
+    stats_year: StockStats,
 }
 
 impl Default for JTrader {
@@ -23,19 +49,24 @@ impl Default for JTrader {
             api_key: get_api_key(),
             selected_stock: Ticker::UNDEFINED,
             doc_stock_data: Vec::new(),
+
+            stats_yesterday: StockStats::default(),
+            stats_week: StockStats::default(),
+            stats_month: StockStats::default(),
+            stats_year: StockStats::default(),
         }
     }
 }
 
 impl JTrader {
-    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         JTrader::default()
     }
 
     async fn get_selected_stock_data(&mut self) {
         let url = daily_open_close::format_url(&self.selected_stock, "2024-12-10", &self.api_key);
 
-        match self.get_request(&url).await {
+        match get_request(&url).await {
             Ok(value) => {
                 println!("{:?}", &value);
                 self.doc_stock_data.push(value);
@@ -43,15 +74,10 @@ impl JTrader {
             Err(e) => println!("Error with sending get request for stock, {}", e),
         }
     }
-
-    async fn get_request(&self, url: &String) -> Result<DailyOpenClose, reqwest::Error> {
-        let response = reqwest::get(url).await?.json::<DailyOpenClose>().await?;
-        Ok(response)
-    }
 }
 
 impl eframe::App for JTrader {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Hello World");
 
@@ -59,13 +85,15 @@ impl eframe::App for JTrader {
             egui::ComboBox::from_label("Select Stock")
                 .selected_text(format!("{:?}", self.selected_stock))
                 .show_ui(ui, |ui| {
-                    ui.selectable_value(&mut self.selected_stock, Ticker::APPL, "APPL")
+                    ui.selectable_value(&mut self.selected_stock, Ticker::APPL, "APPL");
+                    ui.selectable_value(&mut self.selected_stock, Ticker::NVDA, "NVDA");
+                    ui.selectable_value(&mut self.selected_stock, Ticker::GOOG, "GOOG");
+                    ui.selectable_value(&mut self.selected_stock, Ticker::TSLA, "TSLA");
                 });
 
             if ui.add(egui::Button::new("Run")).clicked()
                 && self.selected_stock != Ticker::UNDEFINED
             {
-                self.get_selected_stock_data();
                 println!("{:?}", self.selected_stock);
             }
 
@@ -92,4 +120,9 @@ fn get_api_key() -> String {
         }
     };
     polygon_api_key
+}
+
+async fn get_request(url: &String) -> Result<DailyOpenClose, reqwest::Error> {
+    let response = reqwest::get(url).await?.json::<DailyOpenClose>().await?;
+    Ok(response)
 }
